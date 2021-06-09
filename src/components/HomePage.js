f4ea9f4d1adfab8efc9439d8aa972fd9aed28e0a
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-06-09 18:15:05 -0300

    HomePage backend route implemented

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import PostList from './PostList/PostList';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import PostList from "./PostList/PostList";
+import axios from "axios";
 
 export default function HomePage() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    setPosts([{
-      id: 1,
-      title: 'Hello World',
-      coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-      contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
-      content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
-      commentCount: 2
-    }]);
+    const promisse = axios.get("http://localhost:5000/posts");
+    promisse.then((response) => setPosts(response.data));
+    promisse.catch(() => alert("Não foi possível buscar os posts"));
   }, []);
 
-  return (
-    <PostList name="Daily stories" posts={posts} />
-  );
+  return <PostList name="Daily stories" posts={posts} />;
 }
